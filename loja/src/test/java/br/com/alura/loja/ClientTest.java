ff91ba752ae252c3cfb13f8ef6d94bbf170d499c
oluizleme <o.luizleme@gmail.com>
2022-04-02 10:51:11 -0300

    feat(loja):Criada a primeira classe de testes

---
+++ loja/src/test/java/br/com/alura/loja/ClientTest.java
@@ -0,0 +1,51 @@
+package br.com.alura.loja;
+
+import java.net.URI;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+
+public class ClientTest {
+	
+	private HttpServer server;
+
+	@Before
+	public void startaServidor() {
+		server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void mataServidor() {
+		server.stop();
+	}
+
+	//@Test
+	public void testaQueAConexaoComOServidorFunciona() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://www.mocky.io");
+		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Rua Vergueiro 3185"));
+	}
+	
+	@Test
+	public void testaQueBuscaUmCarrinhoTrazOCarrinhoEsperado() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+}
