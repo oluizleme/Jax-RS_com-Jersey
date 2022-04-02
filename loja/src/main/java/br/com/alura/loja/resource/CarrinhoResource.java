30abf7f4e0fb0f3e1f5780210d9ddbe272679728
oluizleme <o.luizleme@gmail.com>
2022-04-02 11:43:04 -0300

    feat(loja): Implementado o metodo adiciona para HTTP POST, para adicionar novos carrinhos

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.core.MediaType;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import com.thoughtworks.xstream.XStream;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -15,6 +16,14 @@ public class CarrinhoResource {
 	public String busca(@PathParam("id") long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		return carrinho.toXML();
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo) {
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		new CarrinhoDAO().adiciona(carrinho);
+		return "<status>sucesso</status>";
 	}
 
 }
