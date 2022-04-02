4c2a54da5762485902644919293788d7a44641df
oluizleme <o.luizleme@gmail.com>
2022-04-02 12:03:28 -0300

    refactor(loja): Alterada a implementacao para nao retornar um XML mas sim um Response com o status e a location do carrinho inserido; informado o tipo de dado consumido pelo metodo

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -2,10 +2,13 @@ package br.com.alura.loja.resource;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
 import com.thoughtworks.xstream.XStream;
+
+import java.net.URI;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -19,11 +22,12 @@ public class CarrinhoResource {
 	}
 
 	@POST
-	@Produces(MediaType.APPLICATION_XML)
-	public String adiciona(String conteudo) {
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response adiciona(String conteudo) {
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		new CarrinhoDAO().adiciona(carrinho);
-		return "<status>sucesso</status>";
+		URI uri = URI.create("/carrinhos/" + carrinho.getId());
+		return Response.created(uri).build();
 	}
 
 }
